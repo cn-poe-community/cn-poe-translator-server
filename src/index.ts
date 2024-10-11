f3dc614e4a113f7e1cf167c7b73f8ccf30781d3d
me1ting <52079867+me1ting@users.noreply.github.com>
2024-10-11 19:05:54 +0800

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,134 @@
 import { BasicTranslatorFactory } from "cn-poe-translator";
 import express from "express";
 import Assets from "cn-poe-export-db";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import { Requester } from "./requester.js";
 import { GetCharactersQuery } from "./types.js";
 import { transform } from "pob-building-creator";
 import { deflate } from "pako";
 import { Base64 } from "js-base64";
 
-const port = 8005
-const cookie = ""
+const port = 8005;
+const cookie = "";
 
-const requester = new Requester(cookie)
+const requester = new Requester(cookie);
 
 const factory = new BasicTranslatorFactory(Assets);
 const jsonTranslator = factory.getJsonTranslator();
 
-const app = express()
-app.use(bodyParser.json({ limit: '5mb' }))
+const app = express();
+app.use(bodyParser.json({ limit: "5mb" }));
 
-app.get('/character-window/get-characters', (req, res) => {
-    const query = req.query as any as GetCharactersQuery
-    requester.getCharacters(query.accountName, query.realm).then((data) => {
-        res.send(data)
-    }).catch((err) => {
-        console.log("error: ", "get /character-window/get-characters: ", err)
-        res.send("")
+app.get("/character-window/get-characters", (req, res) => {
+  const query = req.query as any as GetCharactersQuery;
+  requester
+    .getCharacters(query.accountName, query.realm)
+    .then((data) => {
+      res.send(data);
     })
-})
+    .catch((err) => {
+      console.log("error: ", "get /character-window/get-characters: ", err);
+      res.send("");
+    });
+});
 
-app.get('/account/view-profile/*', (req, res) => {
-    const slice = req.originalUrl.split("/")
-    const accountName = slice[slice.length - 1]
-    requester.viewProfile(accountName).then((data) => {
-        res.send(data)
-    }).catch((err) => {
-        console.log("error: ", "get /account/view-profile/*: ", err)
-        res.send("")
+app.get("/account/view-profile/*", (req, res) => {
+  const slice = req.originalUrl.split("/");
+  const accountName = slice[slice.length - 1];
+  requester
+    .viewProfile(accountName)
+    .then((data) => {
+      res.send(data);
     })
-})
+    .catch((err) => {
+      console.log("error: ", "get /account/view-profile/*: ", err);
+      res.send("");
+    });
+});
 
 /**
  * Pob cannot recognize UTF8-encoded character names and treats character names as ASCII strings.
  */
 function decodeUtf8Component(url: string): string {
-    const bytes = Buffer.alloc(url.length);
-    for (let i = 0; i < url.length; i++) {
-        const charCode = url.charCodeAt(i);
-        bytes[i] = charCode;
-    }
+  const bytes = Buffer.alloc(url.length);
+  for (let i = 0; i < url.length; i++) {
+    const charCode = url.charCodeAt(i);
+    bytes[i] = charCode;
+  }
 
-    return bytes.toString("utf-8");
+  return bytes.toString("utf-8");
 }
 
-app.get('/character-window/get-passive-skills', (req, res) => {
-    const urlObj = new URL(
-        decodeUtf8Component(req.url),
-        `http://${req.headers.host}`
-    );
-    const params = urlObj.searchParams;
-    const accountName = params.get("accountName");
-    const character = params.get("character");
-    const realm = params.get("realm");
-    requester.getPassiveSkills(accountName!, character!, realm!)
-        .then((data) => {
-            jsonTranslator.translatePassiveSkills(data);
-            res.send(data)
-        })
-        .catch((err) => {
-            console.log("error: ", "get /character-window/get-passive-skills: ", err)
-            res.send("")
-        })
-})
+app.get("/character-window/get-passive-skills", (req, res) => {
+  const urlObj = new URL(
+    decodeUtf8Component(req.url),
+    `http://${req.headers.host}`
+  );
+  const params = urlObj.searchParams;
+  const accountName = params.get("accountName");
+  const character = params.get("character");
+  const realm = params.get("realm");
+  requester
+    .getPassiveSkills(accountName!, character!, realm!)
+    .then((data) => {
+      jsonTranslator.translatePassiveSkills(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log("error: ", "get /character-window/get-passive-skills: ", err);
+      res.send("");
+    });
+});
 
-app.get('/character-window/get-items', (req, res) => {
-    const urlObj = new URL(
-        decodeUtf8Component(req.url),
-        `http://${req.headers.host}`
-    );
-    const params = urlObj.searchParams;
-    const accountName = params.get("accountName");
-    const character = params.get("character");
-    const realm = params.get("realm");
-    requester.getItems(accountName!, character!, realm!)
-        .then((data) => {
-            jsonTranslator.translateItems(data);
-            res.send(data)
-        })
-        .catch((err) => {
-            console.log("error: ", "get /character-window/get-items: ", err)
-            res.send("")
-        })
-})
+app.get("/character-window/get-items", (req, res) => {
+  const urlObj = new URL(
+    decodeUtf8Component(req.url),
+    `http://${req.headers.host}`
+  );
+  const params = urlObj.searchParams;
+  const accountName = params.get("accountName");
+  const character = params.get("character");
+  const realm = params.get("realm");
+  requester
+    .getItems(accountName!, character!, realm!)
+    .then((data) => {
+      jsonTranslator.translateItems(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log("error: ", "get /character-window/get-items: ", err);
+      res.send("");
+    });
+});
 
-app.get('/transform', async (req, res) => {
-    const urlObj = new URL(
-        decodeUtf8Component(req.url),
-        `http://${req.headers.host}`
-    );
-    const params = urlObj.searchParams;
-    const accountName = params.get("accountName");
-    const character = params.get("character");
-    let items = await requester.getItems(accountName!, character!, "pc");
-    let passiveSkills = await requester.getPassiveSkills(accountName!, character!, "pc");
+app.get("/transform", async (req, res) => {
+  const urlObj = new URL(
+    decodeUtf8Component(req.url),
+    `http://${req.headers.host}`
+  );
+  const params = urlObj.searchParams;
+  const accountName = params.get("accountName");
+  const character = params.get("character");
+  let items = await requester.getItems(accountName!, character!, "pc");
+  let passiveSkills = await requester.getPassiveSkills(
+    accountName!,
+    character!,
+    "pc"
+  );
 
-    items = jsonTranslator.translateItems(items);
-    jsonTranslator.translatePassiveSkills(passiveSkills);
+  items = jsonTranslator.translateItems(items);
+  jsonTranslator.translatePassiveSkills(passiveSkills);
 
-    const building = transform(items, passiveSkills);
-    const compressed = deflate(building.toString())
-    let code = Base64.fromUint8Array(compressed);
-    res.send(code.replaceAll("+", "-").replaceAll("/", "_"))
-})
+  const building = transform(items, passiveSkills);
+  const compressed = deflate(building.toString());
+  let code = Base64.fromUint8Array(compressed);
+  res.send(code.replaceAll("+", "-").replaceAll("/", "_"));
+});
 
-process.on('uncaughtException', err => {
-    console.log('caught uncaught exception: ' + err)
-})
+process.on("uncaughtException", (err) => {
+  console.log("caught uncaught exception: " + err);
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
--- src/requester.ts
+++ src/requester.ts
@@ -2,135 +2,133 @@ import axios from "axios";
 
 const TEST_URL = "https://poe.game.qq.com/trade";
 const GET_CHARACTERS_URL =
-    "https://poe.game.qq.com/character-window/get-characters";
+  "https://poe.game.qq.com/character-window/get-characters";
 const VIEW_PROFILE_URL_PREFIX = "https://poe.game.qq.com/account/view-profile/";
 const GET_PASSIVE_SKILLS_URL =
-    "https://poe.game.qq.com/character-window/get-passive-skills";
+  "https://poe.game.qq.com/character-window/get-passive-skills";
 const GET_ITEMS_URL = "https://poe.game.qq.com/character-window/get-items";
 
 /**
  * POE API requester
  */
 export class Requester {
-    private cookie: string;
+  private cookie: string;
 
-    constructor(cookie: string) {
-        this.cookie = cookie;
+  constructor(cookie: string) {
+    this.cookie = cookie;
+  }
+
+  /**
+   * 检查cookie有效性。
+   * @param cookie
+   */
+  public static async isEffectiveCookie(cookie: string): Promise<boolean> {
+    try {
+      await axios.get(TEST_URL, {
+        maxRedirects: 0,
+        headers: {
+          Cookie: cookie,
+        },
+      });
+      return true;
+    } catch (error) {
+      return false;
     }
+  }
 
-    /**
-     * 检查cookie有效性。
-     * @param cookie
-     */
-    public static async isEffectiveCookie(cookie: string): Promise<boolean> {
-        try {
-            await axios.get(TEST_URL, {
-                maxRedirects: 0,
-                headers: {
-                    Cookie: cookie,
-                },
-            });
-            return true;
-        } catch (error) {
-            return false;
-        }
-    }
+  /**
+   * 检查cookie有效性。
+   */
+  public async isEffectiveCookie(): Promise<boolean> {
+    return Requester.isEffectiveCookie(this.cookie);
+  }
 
-    /**
-     * 检查cookie有效性。
-     */
-    public async isEffectiveCookie(): Promise<boolean> {
-        return Requester.isEffectiveCookie(this.cookie);
-    }
+  /**
+   * 获取角色列表信息。
+   *
+   * @param accountName 用户名
+   * @param realm
+   */
+  public async getCharacters(
+    accountName: string,
+    realm: string
+  ): Promise<Array<Character>> {
+    const form = new URLSearchParams();
+    form.append("accountName", accountName);
+    form.append("realm", realm);
 
-    /**
-     * 获取角色列表信息。
-     *
-     * @param accountName 用户名
-     * @param realm
-     */
-    public async getCharacters(
-        accountName: string,
-        realm: string
-    ): Promise<Array<Character>> {
-        const form = new URLSearchParams();
-        form.append("accountName", accountName);
-        form.append("realm", realm);
+    const res = await axios.post(GET_CHARACTERS_URL, form, {
+      headers: {
+        Cookie: this.cookie,
+      },
+    });
 
-        const res = await axios.post(GET_CHARACTERS_URL, form, {
-            headers: {
-                Cookie: this.cookie,
-            },
-        });
+    return res.data;
+  }
 
-        return res.data;
-    }
+  public async viewProfile(accountName: string): Promise<string> {
+    const url = `${VIEW_PROFILE_URL_PREFIX}${encodeURIComponent(accountName)}`;
 
-    public async viewProfile(accountName: string): Promise<string> {
-        const url = `${VIEW_PROFILE_URL_PREFIX}${encodeURIComponent(
-            accountName
-        )}`;
+    const res = await axios.get(url, {
+      headers: {
+        Cookie: this.cookie,
+      },
+    });
 
-        const res = await axios.get(url, {
-            headers: {
-                Cookie: this.cookie,
-            },
-        });
+    return res.data;
+  }
 
-        return res.data;
-    }
+  public async getPassiveSkills(
+    accountName: string,
+    character: string,
+    realm: string
+  ): Promise<PassiveSkills> {
+    const form = new URLSearchParams();
+    form.append("accountName", accountName);
+    form.append("character", character);
+    form.append("realm", realm);
 
-    public async getPassiveSkills(
-        accountName: string,
-        character: string,
-        realm: string
-    ): Promise<PassiveSkills> {
-        const form = new URLSearchParams();
-        form.append("accountName", accountName);
-        form.append("character", character);
-        form.append("realm", realm);
+    const res = await axios.post(GET_PASSIVE_SKILLS_URL, form, {
+      headers: {
+        Cookie: this.cookie,
+      },
+    });
+    return res.data;
+  }
 
-        const res = await axios.post(GET_PASSIVE_SKILLS_URL, form, {
-            headers: {
-                Cookie: this.cookie,
-            },
-        });
-        return res.data;
-    }
+  public async getItems(
+    accountName: string,
+    character: string,
+    realm: string
+  ): Promise<Items> {
+    const form = new URLSearchParams();
+    form.append("accountName", accountName);
+    form.append("character", character);
+    form.append("realm", realm);
 
-    public async getItems(
-        accountName: string,
-        character: string,
-        realm: string
-    ): Promise<Items> {
-        const form = new URLSearchParams();
-        form.append("accountName", accountName);
-        form.append("character", character);
-        form.append("realm", realm);
+    const res = await axios.post(GET_ITEMS_URL, form, {
+      headers: {
+        Cookie: this.cookie,
+      },
+    });
+    return res.data;
+  }
 
-        const res = await axios.post(GET_ITEMS_URL, form, {
-            headers: {
-                Cookie: this.cookie,
-            },
-        });
-        return res.data;
-    }
-
-    public setCookie(cookie: string) {
-        this.cookie = cookie;
-    }
+  public setCookie(cookie: string) {
+    this.cookie = cookie;
+  }
 }
 
 interface Character {
-    class: string;
-    name: string;
-    league: string;
+  class: string;
+  name: string;
+  league: string;
 }
 
 interface PassiveSkills {
-    items: Array<unknown>;
+  items: Array<unknown>;
 }
 
 interface Items {
-    items: Array<unknown>;
+  items: Array<unknown>;
 }
